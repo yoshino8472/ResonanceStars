2a20f6379060e24d9d3b56265f041bd6dabcf7bb
yoshino8472 <y0sh1n0.8472@gmail.com>
2025-11-02 01:02:53 +0900

    GitHub Pages 表示用にdistを配置

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  server: {
-    host: '0.0.0.0',  // Listen on all network interfaces
-    open: true,       // Automatically open browser on start
-    cors: true        // Enable CORS for all requests
-  }
-}
